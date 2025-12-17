11d5812ec8b8e3160d4651ae5a48fb1b347adaa7
wzh <2760080440@qq.com>
2025-12-17 17:22:02 +0800

    原始代码

---
+++ run.sh
@@ -0,0 +1,8 @@
+python run_gaussian_shading.py \
+      --fpr 0.000001 \
+      --channel_copy 1 \
+      --hw_copy 8 \
+      --chacha \
+      --num 1000 \
+      --reference_model ViT-g-14 \
+      --reference_model_pretrain laion2b_s12b_b42k 
